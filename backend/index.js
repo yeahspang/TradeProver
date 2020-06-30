c77ffa846b2a8110d4246a803b83bdfa229c181c
Sjoerd Rievers <github@arcticiced.com>
2020-06-30 16:30:35 +0200

    Added endpoint to validate encrypted trade

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,12 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
+app.post('/trades/verify', (req, res) => {
+    var encryptedCode = req.body.encryptedCode;
+    const trade = proofGenerator.Decrypt(encryptedCode);
+    res.send(trade);
+})
+
 app.post('/trades/proof', (req, res, next) => {
     var apikey = req.body.ApiKey;
     var tradeId = req.body.TradeId;
@@ -42,7 +48,7 @@ app.post('/trades/proof', (req, res, next) => {
                 assetId: asset.assetid
             });
         }
-        let minifiedTrade = new Trade(trade.steamid_other, assets);
+        let minifiedTrade = new Trade(tradeId, trade.steamid_other, assets);
         var proof = proofGenerator.Encrypt(JSON.stringify(minifiedTrade));
         res.send({proof: proof});
     });
@@ -80,5 +86,8 @@ app.post('/trades/table', (req, res, next) => {
         res.send(minifiedTrades);
     });
 });
+
+//const input = "4b24680e9ba2f6299e0b6ceb510b87c605c19389b3d2287fbe3a05c474b61f0c5baebd79c9bfe70dfd4465d291527a0cc87cf1b5eca13b3451b18f8be0d1e3c79694279f0807ed93047f58fa7f66073c25ad97a2ab550385d948a913affecd1b";
+//console.log(proofGenerator.Decrypt(input));
 
 // var proof = proofGenerator.Encrypt(JSON.stringify(minifiedTrades));
