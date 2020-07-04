29f5bd187f8c9bac2c4d2620ae1d99a2955b1f2d
Britt00 <britt.raafs.nl@hotmail.com>
2020-07-05 01:21:17 +0200

    added id search + minor style updates

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Trade } from 'src/app/models/trade';
+import { Asset } from 'src/app/models/asset';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,7 @@ export class HomeComponent implements OnInit {
   trades: Trade[];
   tradeproof: string;
   public userkey: string;
+  public assetIdToProof: number;
   constructor(private api: ApiService) { }
 
   ngOnInit() {
@@ -23,6 +25,16 @@ export class HomeComponent implements OnInit {
     this.api.getTrades(this.userkey).subscribe(resp => {
       this.trades = resp;
     });
+  }
+
+  checkId(assets : Asset[]) : boolean {
+    //return assets[0].assetId.valueOf().toString();
+    for(let a of assets) {
+      if(a.assetId == this.assetIdToProof) {
+        return true;  
+      }
+    }
+    return false;
   }
 
   proof(tradeId: number) {
